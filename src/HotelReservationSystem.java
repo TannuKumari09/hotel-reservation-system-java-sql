bfac017c0dbdd3ff7cadc903db9aa71a1c1aaf35
Tannu Kumari <tannu3132003@gmail.com>
2025-04-22 23:42:58 +0530

    Forth Commit

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 
 public class HotelReservationSystem {
 
-    private static final String url = "jdbc:mysql://127.0.0.1:3306/hotel_db";
+    private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
 
     private static final String username = "root";
 
